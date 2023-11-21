cb50bb2ce6735fe97de000a88efe37320974fe33
reza jafarnezhad <rezajafarnezhad@outlook.com>
2023-11-21 12:28:49 +0330

    int a

--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             string name = "Danial Abedini";
-            int b = 12;
             float q = 12;
             Console.Write("Result: ");
             Danial.Name = name;
             int b = 12;
+            int s = 122;
             Reza.Hi();
             Console.WriteLine(Danial.Name);
             Console.ReadLine();
