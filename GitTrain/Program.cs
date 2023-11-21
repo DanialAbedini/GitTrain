aacdeaec9353f4c56c2dd29d71afc79935ec3722
danialabedini <danialabedini1380@gmail.com>
2023-11-21 10:52:53 +0330

    Add project files.

---
+++ GitTrain/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GitTrain
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Danial Abedini";
+            Danial.Name = name;
+            Console.WriteLine(Danial.Name);
+            Console.ReadLine();
+        }
+    }
+
+   public static class Danial
+    {
+        public static string Name { set; get; }
+
+    }
+}
