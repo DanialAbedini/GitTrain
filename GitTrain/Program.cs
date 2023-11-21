00b12aa256a2a47e25729de3ae824430993d9852
danialabedini <danialabedini1380@gmail.com>
2023-11-21 12:22:03 +0330

    Some Change For Fun

--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             string name = "Danial Abedini";
+            int b = 12;
+            float q = 12;
+            Console.Write("Result: ");
             Danial.Name = name;
             Console.WriteLine(Danial.Name);
             Console.ReadLine();
