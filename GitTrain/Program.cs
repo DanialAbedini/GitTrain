57e823195c9c68b00cd220ad1b5482181e84eda2
reza jafarnezhad <rezajafarnezhad@outlook.com>
2023-11-21 13:07:39 +0330

    Ahmad Update

--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -1,20 +1,23 @@
-﻿namespace GitTrain
+﻿namespace GitTrain;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string name = "Danial Abedini";
+        float q = 12;
+        Console.Write("Result: ");
+        Danial.Name = name;
+        int b = 12;
+        int s = 122;
+        Reza.Hi();
+        for (int i = 0; i < 2000000; i++)
         {
-            string name = "Danial Abedini";
-            float q = 12;
-            Console.Write("Result: ");
-            Danial.Name = name;
-            int b = 12;
-            int s = 122;
-            Reza.Hi();
             Ahmad.HiAhmad();
-            Console.WriteLine(b+s);
-            Console.WriteLine(Danial.Name);
-            Console.ReadLine();
+
         }
+        Console.WriteLine(b + s);
+        Console.WriteLine(Danial.Name);
+        Console.ReadLine();
     }
 }
