65653578c1ecfd5e3d56b356fe02e367c81cda5d
reza jafarnezhad <rezajafarnezhad@outlook.com>
2023-11-21 13:53:52 +0330

    asasas

--- GitTrain/Program.cs
+++ GitTrain/Program.cs
@@ -1,39 +1,42 @@
-﻿using System.Threading.Channels;
+﻿namespace GitTrain;
 
-namespace GitTrain
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string name = "Danial Abedini";
+        float q = 12;
+        Console.Write("Result: ");
+        Danial.Name = name;
+        int b = 12;
+        int s = 122;
+        string gh = "dsad";
+        Console.WriteLine(gh);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("jkadklflsdfsd");
+
+        for (int i = 0; i < 12; i++)
         {
-            string name = "Danial Abedini";
-            float q = 12;
-            Console.Write("Result: ");
-            Danial.Name = name;
-            int b = 12;
-            int s = 122;
-            string gh = "dsad";
-            Console.WriteLine(gh);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("jkadklflsdfsd");
-
-            for (int i = 0; i < 12; i++)
+            Console.WriteLine("Salam");
+            for (int j = 1; j < 3000; j++)
             {
-                Console.WriteLine("Salam");
-                for (int j = 1; j < 3000; j++)
+                Console.WriteLine("Dany");
+                for (int k = 0; k < 22222; k++)
                 {
-                    Console.WriteLine("Dany");
+                    Console.WriteLine("ss");
                 }
-            }
 
-            Reza.Hi();
-            Console.WriteLine(b+s);
-            Console.WriteLine(Danial.Name);
-            Console.ReadLine();
+
+            }
         }
+
+        Reza.Hi();
+        Console.WriteLine(b + s);
+        Console.WriteLine(Danial.Name);
+        Console.ReadLine();
     }
 }
